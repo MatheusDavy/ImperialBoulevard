1b395d7d897fed04a3981d8d56b39f66c185b4a9
Matheus Davy <matheusdavy17@gmail.com>
2023-07-05 18:36:16 -0300

    FEAT: header

--- resources/views/site/layout/header.blade.php
+++ resources/views/site/layout/header.blade.php
@@ -1,5 +1,11 @@
+<header id="header" class="header">
+    <div class="header--container">
+        <a href="#home">
+            <img class="logo" src="{{ asset('site/img/Header/boulevard-logo.png') }}" alt="Logo Boulevard">
+        </a>
 
-
-<header id="header">
-	
+        <button class="menu-button">
+            <span></span><span></span><span></span>
+        </button>
+    </div>
 </header>
