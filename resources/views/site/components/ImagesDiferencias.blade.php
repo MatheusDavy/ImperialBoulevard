1c5cf1f7511484f9d99d02519c56df8cdec14c90
Matheus Davy <matheusdavy17@gmail.com>
2023-07-11 09:26:12 -0300

    FIX: images & scrollbars

--- resources/views/site/components/ImagesDiferencias.blade.php
+++ resources/views/site/components/ImagesDiferencias.blade.php
@@ -1,8 +1,8 @@
-<img src="{{ assetJson([$fifthSection->folder,$gallery->image]) }}" alt="{{imgAltJson($gallery->image)}}" 
+<img loading="lazy" src="{{ assetJson([$fifthSection->folder,$gallery->image]) }}" alt="{{imgAltJson($gallery->image)}}" 
 										title="{{imgTitleJson($gallery->image)}}">
 <div class="image-description">
-    <button class="image-description--zoom images--zoom" data-src="{{ assetJson([$fifthSection->folder,$gallery->image]) }}">
-        <img src="{{ asset('site/img/Home/Differentials/zoom.svg') }}" alt="">
+    <button class="image-description--zoom images--zoom" data-src="{{ assetJson([$fifthSection->folder,$gallery->image]) }}" aria-label="Ver imagem ampliada">
+        <img loading="lazy" src="{{ asset('site/img/Home/Differentials/zoom.svg') }}" alt="">
     </button>
 
     <p class="image-description--description">
--- resources/views/site/components/Modal/zoom_modal.blade.php
+++ resources/views/site/components/Modal/zoom_modal.blade.php
@@ -1,9 +1,9 @@
 <div id="modal-zoom-images">
 
-    <button id="btn-close__zoom-modal">
+    <button id="btn-close__zoom-modal" aria-label="Fechar imagem ampliada">
         <i class="fa-solid fa-xmark"></i>
     </button>
 
-    <img id="image__zoom-modal" src="" alt="">
+    <img id="image__zoom-modal" src="" alt="Imagem ampliada">
 
 </div>
