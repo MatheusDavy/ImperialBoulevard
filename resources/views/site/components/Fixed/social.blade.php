500d9cc62931be606ed7e97985c76234cac8e72c
Rafael Borges <rboges13795@gmail.com>
2023-07-13 11:26:31 -0300

    alterando nome do folder de icons para Icons

--- resources/views/site/components/Fixed/social.blade.php
+++ resources/views/site/components/Fixed/social.blade.php
@@ -1,14 +1,14 @@
 <div class="social">
     <a href="#" target="_blank" class="social__icon">
-        <img class="logo" src="{{ asset('site/img/icons/Vector-3.svg') }}" alt="youtube-icon">
+        <img class="logo" src="{{ asset('site/img/Icons/Vector-3.svg') }}" alt="youtube-icon">
     </a>
     <a href="https://wa.me/5554996368858" target="_blank" class="social__icon">
-        <img class="logo" src="{{ asset('site/img/icons/Vector-2.svg') }}" alt="wpp-icon">
+        <img class="logo" src="{{ asset('site/img/Icons/Vector-2.svg') }}" alt="wpp-icon">
     </a>
     <a href="https://www.instagram.com/boulevardconvention/" target="_blank" class="social__icon">
-        <img class="logo" src="{{ asset('site/img/icons/Vector-1.svg') }}" alt="facebook-icon">
+        <img class="logo" src="{{ asset('site/img/Icons/Vector-1.svg') }}" alt="facebook-icon">
     </a>
     <a href="https://www.facebook.com/boulevardconvention" target="_blank" class="social__icon">
-        <img class="logo" src="{{ asset('site/img/icons/Vector.svg') }}" alt="instagram-icon">
+        <img class="logo" src="{{ asset('site/img/Icons/Vector.svg') }}" alt="instagram-icon">
     </a>
 </div>
